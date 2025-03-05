e67408f26c9d5f951fa32e51b58fd9e3cf85cebd
Amit Sah <amitsah04v@gmail.com>
2025-03-05 13:25:03 +0530

    fixed

--- src/app/components/codeCopy.tsx
+++ src/app/components/codeCopy.tsx
@@ -6,10 +6,10 @@ interface IProps {
 }
 const CodeCopy = ({ setCloseCodeBtn }: IProps) => {
   return (
-    <div className="top-8 left-10 z-40 fixed bg-white/10 shadow-black shadow-inner backdrop-blur-[6rem] border-2 border-white/15 rounded-md w-[95vw] h-[90vh]">
+    <div className="top-8 left-10 z-40 fixed bg-white/10 shadow-black shadow-inner backdrop-blur-[6rem] border border-white/20 border-t-neutral-600 rounded-md w-[95vw] h-[90vh]">
       <button
         onClick={() => setCloseCodeBtn(false)}
-        className="top-2 right-2 absolute shadow-gray-50/5 shadow-inner hover:shadow-red-300/25 p-1 hover:text-red-500 hover:scale-110 active:scale-75 transition-all cursor-pointer"
+        className="top-2 right-2 absolute shadow-gray-50/10 shadow-inner hover:shadow-red-300/25 p-1 rounded-full hover:text-red-500 hover:scale-110 active:scale-75 transition-all cursor-pointer"
       >
         <X />
       </button>{" "}
