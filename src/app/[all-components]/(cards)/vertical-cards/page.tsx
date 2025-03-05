c9b336831f26b37d3945cf2b6953611b067f057d
Amit Sah <amitsah04v@gmail.com>
2025-03-05 13:13:18 +0530

    fixing the copy btn

--- src/app/[all-components]/(cards)/vertical-cards/page.tsx
+++ src/app/[all-components]/(cards)/vertical-cards/page.tsx
@@ -1,14 +1,19 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { AvatarCards } from "./components";
+import CodeCopy from "../../../components/codeCopy";
 
 const Page = () => {
+  const [copyCode, setCopyCode] = useState<boolean>(false);
+  console.log(copyCode)
   return (
     <div className="w-full">
       <h1 className="flex justify-center w-full font-roboto text-5xl">
         Vertical Cards
       </h1>
-      <AvatarCards type="center-aligned" />
+      <AvatarCards type="center-aligned" setOpenCopyCode={setCopyCode}/>
+
+      {copyCode && <CodeCopy setCloseCodeBtn={setCopyCode} />}
     </div>
   );
 };
